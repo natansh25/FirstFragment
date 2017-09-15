f9a3387301d7213e6781ad6593941eb1f55726ab
Natansh <natansh25@gmail.com>
2017-09-15 12:11:52 -0400

    add and remove of a fragment programatically

--- app/src/main/java/com/example/natan/firstfragment/MainActivity.java
+++ app/src/main/java/com/example/natan/firstfragment/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.natan.firstfragment;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+    public void clickHandler(View view) {
+
+
+        SimpleFragment fragment = new SimpleFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentContainer,fragment)
+                .commit();
+
+    }
+
+    public void clickHandlerRm(View view) {
+        Fragment fragment=getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(fragment)
+                    .commit();
+        }
+
+
+
     }
 }
